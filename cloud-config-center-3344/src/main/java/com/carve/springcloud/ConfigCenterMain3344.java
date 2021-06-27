d06c0b4598ad3faeeb53b9fcec7ae93260f3415a
carve <cctv16@88.com>
2021-06-27 14:00:16 +0800

    Default Changelist

---
+++ cloud-config-center-3344/src/main/java/com/carve/springcloud/ConfigCenterMain3344.java
@@ -0,0 +1,20 @@
+package com.carve.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * Function:
+ * ClassName: ConfigCenterApplication3344
+ *
+ * @author lmb on 2021/6/27 10:11
+ */
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigCenterMain3344 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigCenterMain3344.class,args);
+    }
+}
